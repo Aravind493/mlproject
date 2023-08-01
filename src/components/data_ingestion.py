b68122bb38d30c88d91aa7b06bfdbc5d7ae097d6
Aravind439 <tammineediaravind00@gmail.com>
2023-08-01 12:18:58 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,11 @@ from src.logger import logging
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 ##THESE are  the nputs for the data ingetion it shows whre to store the data
 @dataclass
@@ -25,7 +29,8 @@ class DataIngestion:
         try:
             df=pd.read_csv('notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path))
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
@@ -46,12 +51,14 @@ class DataIngestion:
             raise CustomException(e,sys)
 
 
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
+
             
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initate_model_trainer(train_arr,test_arr))
